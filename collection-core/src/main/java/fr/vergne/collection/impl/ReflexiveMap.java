e3a3f86884287060b32d660b4c6f08c98f2f2c08
vergne <vergne@fbk.eu>
2014-04-22 17:32:19 +0200

    Add ReflexiveMap.

---
+++ collection-core/src/main/java/fr/vergne/collection/impl/ReflexiveMap.java
@@ -0,0 +1,104 @@
+package fr.vergne.collection.impl;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ReflexiveMap<Key, Value> implements Map<Key, Value> {
+
+	private final Map<Key, Value> keyValue;
+	private final Map<Value, Key> valueKey;
+	private final ReflexiveMap<Value, Key> reversed;
+
+	public ReflexiveMap() {
+		keyValue = new LinkedHashMap<Key, Value>();
+		valueKey = new LinkedHashMap<Value, Key>();
+		reversed = new ReflexiveMap<Value, Key>(this);
+	}
+
+	private ReflexiveMap(ReflexiveMap<Value, Key> reflex) {
+		keyValue = reflex.valueKey;
+		valueKey = reflex.keyValue;
+		reversed = reflex;
+	}
+
+	@Override
+	public void clear() {
+		keyValue.clear();
+		valueKey.clear();
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return keyValue.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return keyValue.containsValue(value);
+	}
+
+	@Override
+	public Set<Entry<Key, Value>> entrySet() {
+		return keyValue.entrySet();
+	}
+
+	@Override
+	public Value get(Object key) {
+		return keyValue.get(key);
+	}
+
+	public Value getValueFrom(Key key) {
+		return keyValue.get(key);
+	}
+
+	public Key getKeyFrom(Value value) {
+		return valueKey.get(value);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return keyValue.isEmpty();
+	}
+
+	@Override
+	public Set<Key> keySet() {
+		return keyValue.keySet();
+	}
+
+	@Override
+	public Value put(Key key, Value value) {
+		valueKey.remove(keyValue.get(key));
+		valueKey.put(value, key);
+		return keyValue.put(key, value);
+	}
+
+	@Override
+	public void putAll(Map<? extends Key, ? extends Value> m) {
+		for (Entry<? extends Key, ? extends Value> entry : m.entrySet()) {
+			put(entry.getKey(), entry.getValue());
+		}
+	}
+
+	@Override
+	public Value remove(Object key) {
+		Value removed = keyValue.remove(key);
+		valueKey.remove(removed);
+		return removed;
+	}
+
+	@Override
+	public int size() {
+		return keyValue.size();
+	}
+
+	@Override
+	public Collection<Value> values() {
+		return keyValue.values();
+	}
+
+	public ReflexiveMap<Value, Key> reverse() {
+		return reversed;
+	}
+}
---
+++ collection-core/src/test/java/fr/vergne/collection/impl/ReflexiveMapTest.java
@@ -0,0 +1,84 @@
+package fr.vergne.collection.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReflexiveMapTest {
+
+	@Test
+	public void test() {
+		ReflexiveMap<String, Integer> map = new ReflexiveMap<String, Integer>();
+		assertEquals(0, map.size());
+
+		map.put("1", 1);
+		map.put("2", 2);
+		map.put("3", 3);
+
+		assertEquals(3, map.size());
+		assertEquals((Integer) 1, map.get("1"));
+		assertEquals((Integer) 2, map.get("2"));
+		assertEquals((Integer) 3, map.get("3"));
+		assertEquals(null, map.get("4"));
+
+		assertEquals((Integer) 1, map.getValueFrom("1"));
+		assertEquals((Integer) 2, map.getValueFrom("2"));
+		assertEquals((Integer) 3, map.getValueFrom("3"));
+		assertEquals(null, map.getValueFrom("4"));
+
+		assertEquals("1", map.getKeyFrom(1));
+		assertEquals("2", map.getKeyFrom(2));
+		assertEquals("3", map.getKeyFrom(3));
+		assertEquals(null, map.getKeyFrom(4));
+
+		map.remove("2");
+
+		assertEquals((Integer) 1, map.getValueFrom("1"));
+		assertEquals(null, map.getValueFrom("2"));
+		assertEquals((Integer) 3, map.getValueFrom("3"));
+		assertEquals(null, map.getValueFrom("4"));
+
+		assertEquals("1", map.getKeyFrom(1));
+		assertEquals(null, map.getKeyFrom(2));
+		assertEquals("3", map.getKeyFrom(3));
+		assertEquals(null, map.getKeyFrom(4));
+
+		ReflexiveMap<Integer, String> reverse = map.reverse();
+
+		assertEquals((Integer) 1, reverse.getKeyFrom("1"));
+		assertEquals(null, reverse.getKeyFrom("2"));
+		assertEquals((Integer) 3, reverse.getKeyFrom("3"));
+		assertEquals(null, reverse.getKeyFrom("4"));
+
+		assertEquals("1", reverse.getValueFrom(1));
+		assertEquals(null, reverse.getValueFrom(2));
+		assertEquals("3", reverse.getValueFrom(3));
+		assertEquals(null, reverse.getValueFrom(4));
+
+		reverse.put(2, "2");
+
+		assertEquals((Integer) 1, reverse.getKeyFrom("1"));
+		assertEquals((Integer) 2, reverse.getKeyFrom("2"));
+		assertEquals((Integer) 3, reverse.getKeyFrom("3"));
+		assertEquals(null, reverse.getKeyFrom("4"));
+
+		assertEquals("1", reverse.getValueFrom(1));
+		assertEquals("2", reverse.getValueFrom(2));
+		assertEquals("3", reverse.getValueFrom(3));
+		assertEquals(null, reverse.getValueFrom(4));
+
+		assertEquals((Integer) 1, map.getValueFrom("1"));
+		assertEquals((Integer) 2, map.getValueFrom("2"));
+		assertEquals((Integer) 3, map.getValueFrom("3"));
+		assertEquals(null, map.getValueFrom("4"));
+
+		assertEquals("1", map.getKeyFrom(1));
+		assertEquals("2", map.getKeyFrom(2));
+		assertEquals("3", map.getKeyFrom(3));
+		assertEquals(null, map.getKeyFrom(4));
+
+		assertEquals(reverse, map.reverse());
+		assertEquals(map, reverse.reverse());
+	}
+
+}
